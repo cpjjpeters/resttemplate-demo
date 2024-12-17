5cc23c5bcfddcf09afcaa988dacf222f75c883bf
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-12-17 15:47:56 +0100

    removed almost all baeldung stuff

--- src/main/java/ipeters/resttemplate_demo/resttemplate/configuration/FooController.java
+++
@@ -1,7 +0,0 @@
-package ipeters.resttemplate_demo.resttemplate.configuration;
-
-/* carlpeters created on 17/12/2024 inside the package - ipeters.resttemplate_demo.resttemplate.configuration */
-public class FooController {
-
-// https://github.com/eugenp/tutorials/blob/master/spring-web-modules/spring-resttemplate/src/main/java/com/baeldung/resttemplate/configuration/FooController.java
-}
